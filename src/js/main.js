58d7128039bb37bea9f84e1613e7fcba02d9d70e
y-nagai0725 <y.nagai0725@gmail.com>
2026-08-26 18:50:05 +0900

    feat: ベースhtml作成、横スクロールテスト

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 import '../scss/style.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+// =========================================
+// 横スクロールのテスト実装
+// =========================================
+
+let mm = gsap.matchMedia();
+
+mm.add("(width >= 1024px)", () => {
+  const scrollContainer = document.querySelector('.js-scroll-container');
+  const track = document.querySelector('.js-horizontal-track');
+
+  if (!scrollContainer || !track) return;
+
+  const scrollWidth = () => track.clientWidth - window.innerWidth;
+
+  gsap.to(track, {
+    x: () => -scrollWidth(),
+    ease: "none",
+    scrollTrigger: {
+      trigger: scrollContainer,
+      pin: true,
+      start: "top top",
+      end: () => "+=" + scrollWidth(),
+      scrub: 1,
+      invalidateOnRefresh: true,
+    }
+  });
+});
